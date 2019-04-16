ff5ba7bba0af2a87636d693cd1997fa8a3185fbd
yfakariya <yfakariya@gmail.com>
2019-04-16 23:39:23 +0900

    WIP: Add test

---
+++ test/Wisteria.IO.LayeredStreams.Tests/LayeredStreamTest.cs
@@ -0,0 +1,16 @@
+// Copyright © FUJIWARA, Yusuke 
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using NUnit.Framework;
+using System;
+
+namespace Wisteria.IO.LayeredStreams
+{
+	[TestFixture]
+	public class LayeredStreamTest
+	{
+		[Test]
+		public void NotImpl() => Assert.Inconclusive();
+	}
+}
