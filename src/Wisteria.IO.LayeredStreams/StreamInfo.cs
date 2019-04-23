ba3e917c775df177ed292301cc757eca84cf0903
yfakariya <yfakariya@gmail.com>
2019-04-23 20:55:18 +0900

    FIX

--- src/Wisteria.IO.LayeredStreams/StreamInfo.cs
+++ src/Wisteria.IO.LayeredStreams/StreamInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © FUJIWARA, Yusuke 
+// Copyright © FUJIWARA, Yusuke 
 // This file is licensed to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
